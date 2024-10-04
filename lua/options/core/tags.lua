f6e03c1e4ab18eaf39ffc6f3b72ffac4574afe12
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-10-04 10:00:32 +0300

    add 'tags' options

---
+++ lua/options/core/tags.lua
@@ -0,0 +1,7 @@
+---- tags ----
+
+-- use binary searching in tags files
+vim.opt.tagbsearch = true
+
+-- how to handle case when searching in tags files (follow the 'smartcase' and 'ignorecase' options)
+vim.opt.tagcase = "followscs"
