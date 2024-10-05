03747901723089491d2640de1c3790549056564c
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-10-05 10:00:46 +0300

    add 'editing text' options

---
+++ lua/options/editing/editing.lua
@@ -0,0 +1,19 @@
+---- editing text ----
+
+-- persistent undo even after you close a file and re-open it
+vim.opt.undofile = true
+
+-- correctly break multi-byte characters such as CJK
+vim.opt.formatoptions:append("mM")
+
+-- better completion experience
+vim.opt.completeopt = { "menu", "menuone", "noselect", "preview" }
+
+-- maximum height of the popup menu
+vim.opt.pumheight = 10
+
+-- list of pairs that match for the "%" command
+vim.opt.matchpairs:append([[<:>,「:」,『:』,【:】,“:”,‘:’,《:》]])
+
+-- recognized for CTRL-A and CTRL-X commands
+vim.opt.nrformats = { "alpha", "octal", "hex", "bin" }
