0916613dca74de5f382d72102b02e9a93d8310d5
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-10-05 15:21:16 +0300

    add 'reading and writing files' options

---
+++ lua/options/files/read-write.lua
@@ -0,0 +1,7 @@
+---- reading and writing files ----
+
+-- write a backup file before overwriting a file
+vim.opt.writebackup = false
+
+-- keep a backup after overwriting a file
+vim.opt.backup = false
