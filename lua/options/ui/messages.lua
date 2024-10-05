07dce9e676fdf70bd0123496b0692ae69cc3ecad
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-10-05 07:34:39 +0300

    add 'messages and info' options

---
+++ lua/options/ui/messages.lua
@@ -0,0 +1,13 @@
+---- messages and info ----
+
+-- don't show the command in the last line
+vim.opt.showcmd = false
+
+-- disable showing current mode on command line since statusline plugins can show it
+vim.opt.showmode = false
+
+-- don't show the ruler
+vim.opt.ruler = false
+
+-- confirm to save changes before exiting modified buffer
+vim.opt.confirm = true
