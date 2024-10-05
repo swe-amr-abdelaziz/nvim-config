b2baa9173bff309ba0e161f57d42c8e5803c2bb9
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-10-05 15:29:27 +0300

    add 'the swap file' options

---
+++ lua/options/files/swap.lua
@@ -0,0 +1,7 @@
+---- the swap file ----
+
+-- use a swap file for this buffer
+vim.opt.swapfile = false
+
+-- used for CursorHold event (for document highlighting detection)
+vim.opt.updatetime = 100
