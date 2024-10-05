2d65184b225526e0830fee58fc2661d387290f48
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-10-05 14:57:03 +0300

    add 'mapping' options

---
+++ lua/options/editing/mapping.lua
@@ -0,0 +1,19 @@
+---- mapping ----
+
+-- change leader to a space
+vim.g.mapleader = " "
+
+-- change localleader to a space
+vim.g.maplocalleader = " "
+
+-- allow timing out halfway into a mapping
+vim.opt.timeout = true
+
+-- allow timing out halfway into a key code
+vim.opt.ttimeout = true
+
+-- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.timeoutlen = 1000
+
+-- time to wait for a key code sequence to complete (in milliseconds)
+vim.opt.ttimeoutlen = 10
