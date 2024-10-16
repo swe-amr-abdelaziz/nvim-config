07550e6e9e4a5b966957f34c3e0bc3222b7be93f
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-10-16 21:27:00 +0300

    add lazy.nvim

---
+++ lua/options/core/lazy.lua
@@ -0,0 +1,56 @@
+---- lazy.nvim plugin manager ----
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- setup global mapleader before loading lazy.nvim
+require("options.editing.mapping")
+
+require("lazy").setup({
+  spec = require("utils.general").lazy_imports_recursively("plugins"),
+  install = {
+    colorscheme = { "habamax" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
+  ui = {
+    border = "rounded",
+    icons = {
+      ft = "",
+      loaded = "",
+      not_loaded = "",
+    },
+    title = "Plugin Manager",
+    title_pos = "center",
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
