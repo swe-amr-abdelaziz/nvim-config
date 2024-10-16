e523513951e9161a47351dde33d4c1a109d5b0f4
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2024-10-16 21:28:05 +0300

    add lazy_imports_recursively function

--- lua/utils/general.lua
+++ lua/utils/general.lua
@@ -19,4 +19,37 @@ M.require_recursively = function(folder)
   load_files(folder_path)
 end
 
+M.lazy_imports_recursively = function(folder)
+  local folder_path = vim.fn.stdpath("config") .. "/lua/" .. folder
+  local imports = {}
+
+  local function contains_lua_file(dir)
+    local files = vim.fn.readdir(dir)
+    for _, file in ipairs(files) do
+      if file:match("%.lua$") then
+        return true
+      end
+    end
+    return false
+  end
+
+  local function scan_folder(dir)
+    local files = vim.fn.readdir(dir)
+    if contains_lua_file(dir) then
+      local relative_dir = dir:match("lua/(.*)$"):gsub("/", ".")
+      table.insert(imports, { import = relative_dir })
+    end
+    for _, file in ipairs(files) do
+      local full_path = dir .. "/" .. file
+      if vim.fn.isdirectory(full_path) == 1 then
+        scan_folder(full_path)
+      end
+    end
+  end
+
+  scan_folder(folder_path)
+
+  return imports
+end
+
 return M
