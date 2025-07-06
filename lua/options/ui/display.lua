2e31f2dbfe954882d3e3ec746a2dc20e5bd0eb4d
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-07-06 07:09:54 +0300

    feat(options): update display options

--- lua/options/ui/display.lua
+++ lua/options/ui/display.lua
@@ -3,6 +3,9 @@
 -- number of lines to scroll for CTRL-U and CTRL-D (default half the window height, scroll = 0)
 vim.opt.scroll = 0
 
+-- scrolls screen line-by-line (instead of jumpy half/full page)
+vim.opt.smoothscroll = true
+
 -- minimum lines to keep above and below cursor when scrolling
 vim.opt.scrolloff = 8
 
@@ -16,7 +19,7 @@ vim.opt.linebreak = true
 vim.opt.breakindent = true
 
 -- character to show before the lines that have been wrapped
-vim.opt.showbreak="↳ "
+vim.opt.showbreak = "↳ "
 
 -- minimal number of columns to scroll horizontally
 vim.opt.sidescroll = 1
@@ -40,7 +43,20 @@ vim.opt.fillchars = {
 vim.opt.cmdheight = 1
 
 -- useful when executing macros.
-vim.opt.lazyredraw = false
+vim.opt.lazyredraw = true
+
+-- show <Tab> as ^I and end-of-line as $
+vim.opt.list = true
+
+-- list of strings used for list mode
+vim.opt.listchars = {
+  tab = '»·',
+  trail = '·',
+  nbsp = '␣',
+  extends = '⟩',
+  precedes = '⟨',
+  space = ' ',
+}
 
 -- show the line number for each line
 vim.opt.number = true
